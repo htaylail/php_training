7a61a7e77860e8676b21e1dc3402ced06f04d908
htaylail <44107539+htaylail@users.noreply.github.com>
2022-02-25 20:49:32 +0630

    Update qrcode.php

--- tutorials/tutorial_7/qrcode.php
+++ tutorials/tutorial_7/qrcode.php
@@ -8,13 +8,21 @@ if (isset($_POST['inputText'])) {
         $text = $_POST['name'];
         $text .= $_POST['email'];
         $text .= $_POST['address'];
-        mkdir('images');
         $path = "images/";
-        $qrCodeName = $_POST['filename'];
-        $file = $path . uniqid($qrCodeName) . ".png";
-        // to create & display -> png($text, $file, ECC_LEVEL, Pixel_Size, Frame_size)
-        QRcode::png($text, $file, 'L', 10, 2);
-        echo "<img src='" . $file . "'>";
+        // To check folder path is exist or not
+        if (!is_dir($path)) {
+            mkdir('images');
+            $qrCodeName = $_POST['filename'];
+            $file = $path . uniqid($qrCodeName) . ".png";
+            QRcode::png($text, $file, 'L', 10, 2);
+            echo "<img src='" . $file . "'>";
+        } else {
+            $qrCodeName = $_POST['filename'];
+            $file = $path . uniqid($qrCodeName) . ".png";
+            // to create & display -> png($text, $file, ECC_LEVEL, Pixel_Size, Frame_size)
+            QRcode::png($text, $file, 'L', 10, 2);
+            echo "<img src='" . $file . "'>";
+        }
     }
 }
 ?>
